f4665b51707bfd191bfa11fd2e11ad13b0126692
CHRISTIANAGUIRRE16 <98773206+CHRISTIANAGUIRRE16@users.noreply.github.com>
2022-04-22 18:06:34 -0500

    CONTROLADOR Y RUTA MODULO COMENTARIOS

--- ISNTC.ADMIN/SRC/rutas/comentariosRutas.js
+++ ISNTC.ADMIN/SRC/rutas/comentariosRutas.js
@@ -4,6 +4,9 @@ const rutas=express.Router()
 const { mostrar}=require("../Controladores/comentariosControlador")
 
 rutas.get('/comentariosAgregar/', mostrar)
+rutas.post('/comentariosLista/', enviar)
+rutas.get('/comentariosEditar/:id', traer)
+
 
 
 module.exports= rutas
